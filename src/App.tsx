eb43d6186307e26f7661c42f14a808ba4e415da1
pshabanov <pshabanov@nurtelecom.kg>
2023-02-01 11:15:07 +0600

    routing, page-chunking

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { Counter } from './components/Counter'
+import React, { Suspense } from 'react'
 import './index.scss'
+import { Link, Route, Routes } from 'react-router-dom'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
 
 const App = () => {
     return (
-        <div className='app'>
-            <Counter/>
+        <div className="app">
+            <Link to={ '/' }>Главная</Link>
+            <Link to={ '/about' }>О нас</Link>
+            <Suspense fallback={ <div>Loading...</div> }>
+                <Routes>
+                    <Route path={ '/about' } element={ <AboutPageAsync/> }/>
+                    <Route path={ '/' } element={ <MainPageAsync/> }/>
+                </Routes>
+            </Suspense>
         </div>
     )
 }
