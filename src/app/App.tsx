e6ae3809cacdfc88c82312a6f98b6bd380387d76
pshabanov <pshabanov@nurtelecom.kg>
2023-02-01 18:09:14 +0600

    FSD architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from 'react'
 import './styles/index.scss'
 import { Link, Route, Routes } from 'react-router-dom'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { useTheme } from './theme/useTheme'
-import { ClassNames} from './helpers/classNames/classNames'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { MainPage } from 'pages/MainPage'
+import { AboutPage } from 'pages/AboutPage'
+import { ClassNames } from 'shared/lib/classNames/classNames'
+
 
 const App = () => {
 
@@ -17,8 +18,8 @@ const App = () => {
             <Link to={ '/about' }>О нас</Link>
             <Suspense fallback={ <div>Loading...</div> }>
                 <Routes>
-                    <Route path={ '/about' } element={ <AboutPageAsync/> }/>
-                    <Route path={ '/' } element={ <MainPageAsync/> }/>
+                    <Route path={ '/about' } element={ <AboutPage/> }/>
+                    <Route path={ '/' } element={ <MainPage/> }/>
                 </Routes>
             </Suspense>
         </div>
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,20 +1,20 @@
 import React, { FC, useMemo, useState } from 'react'
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext'
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.DARK
 
-const ThemeProvider:FC = ({children}) => {
+const ThemeProvider: FC = ({children}) => {
 
     const [theme, setTheme] = useState<Theme>(defaultTheme)
 
-    const defaultProps = useMemo(()=>({
-            theme: theme,
-            setTheme: setTheme
+    const defaultProps = useMemo(() => ({
+        theme: theme,
+        setTheme: setTheme,
     }), [theme])
 
     return (
-        <ThemeContext.Provider value={defaultProps}>
-            {children}
+        <ThemeContext.Provider value={ defaultProps }>
+            { children }
         </ThemeContext.Provider>
     )
 }
